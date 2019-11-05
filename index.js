eb34cbaac05b959c2fdd16db7766e4dcecb4e018
Rick Brown <ricky.brown.00@gmail.com>
2019-11-05 16:02:26 +0000

    database connecting locally

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-app.get('/', (req, res) => res.send(`<h1>../</h1>`))
+const PORT = process.env.PORT || 3000
+let CONNECTED = 'Not connected to database.'
+
+// Connect to mongoDB
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@basic-backend-api-nnvyx.mongodb.net/test?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+  )
+  .then(data => {
+    CONNECTED = 'We are now connected to the database'
+    console.log(
+      `Welcome ${process.env.MONGO_USER}, You are now connected to ${process.env.MONGO_DB}.`
+    )
+  })
+  .catch(err => console.log(err))
+
+mongoose.Promise = global.Promise
+
+app.get('/', (req, res) => res.send(CONNECTED))
 
 app.get('/test-route', (req, res) => res.send(`<h1>../test-route</h1>`))
 
